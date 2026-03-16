ca6471e5fca37095b6493a1470fef5123cf3c72f
whiskaz112 <wisapat2546@gmail.com>
2026-03-16 23:19:19 +0700

    WIP

--- src/modules/home/controller.py
+++ src/modules/home/controller.py
@@ -9,14 +9,14 @@ from .service import get_top_pledged_campaigns
 from .schema import CategoryStat
 from .service import get_category_stats_service
 
-from .schema import HomePostOut
+from .schema import HomeCampaignOut
 
 router = APIRouter(prefix="/home", tags=["home"])
 
 
 @router.get(
     "/top-pledged-campaigns",
-    response_model=list[HomePostOut],
+    response_model=list[HomeCampaignOut],
 )
 def get_top_pledged_campaigns_controller(
     limit: int = 10,
